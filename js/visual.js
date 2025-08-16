9e145a23468525f97b5aa6d56fe0202d90eb4e51
9ouo9pp <9ouo9pp@gmail.com>
2025-08-16 21:13:53 +0900

    visual 작성중

---
+++ js/visual.js
@@ -0,0 +1,16 @@
+const swiperVisual = new Swiper(".swiperVisual", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
